7a2e77cdc1c35a0899c02108060b0ca8c43e51c8
Kevin NP <kp728155@gmail.com>
2025-12-09 09:51:08 -0600

    Delete documentacion/10- Codigo final directory

--- documentacion/10- Codigo final/src/main/java/com/saludintegral/Clinica/repositorio/LogActividadRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.saludintegral.Clinica.repositorio;
-
-import com.saludintegral.Clinica.modelos.LogActividad;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LogActividadRepositorio extends JpaRepository<LogActividad, Integer> {
-
-}
-
